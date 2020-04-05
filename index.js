2bcf36e3be6170cc550259f923349aa4cc96302e
umairx97 <umair.bajwa@hotmail.com>
2020-04-06 02:18:37 +0500

    fix: fix server logics

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const http = require('http')
 const request = require('request')
-     ,url = 'https://jsonplaceholder.typicode.com/users'
+const url = 'https://jsonplaceholder.typicode.com/users'
 const PORT = 3003
 
 const usersObj = {
   users: []
 }
 
-function requestHandler() {
-request(url, (error, response)=> {
-  if (!error && response.statusCode === 200) {
-    const { users } = usersObj.users
-    const response = users
-    // const newUsers = JSON.parse(users)
-    // const response = JSON.parse(body)
-    users.map((item) => {
-     response.end(item.id)
+const getUsers = () => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response) => {
+      if (!error && response.statusCode === 200) {
+        let { users } = usersObj
+        users = [...users, response.body]
+        resolve(users)
+      } else {
+        reject(new Error('Server Error, please try again'))
+      }
     })
-    console.log(response)
-  } else {
-    console.log("Got an error: ", error, ", status code: ", response.statusCode)
+  })
+}
+
+async function requestHandler (req, res) {
+  try {
+    const data = await getUsers()
+    res.end(JSON.stringify(data))
+  } catch (err) {
+    res.end(err.message)
   }
-})
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(PORT, () => {
-  console.log('server is running on port 3003')
+  console.log(`Server running on http://localhost:${PORT}`)
 })
